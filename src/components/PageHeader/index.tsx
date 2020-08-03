7485cf27d25130e6a445d7833d9f110596cb6cf8
lucas.paszinski <lucas.paszinski@altus.com.br>
2020-08-03 20:31:53 -0300

    Added page header component

---
+++ src/components/PageHeader/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './styles.css'
+
+import { Link } from 'react-router-dom'
+
+import backIcon from '../../assets/images/icons/back.svg'
+import logoImg from '../../assets/images/logo.svg'
+
+interface PageHeaderProps
+{
+    title: string
+}
+
+const PageHeader: React.FC<PageHeaderProps> = (props) => 
+{
+    return (
+        <div className="page-header">
+            <div className="top-bar-container">
+                <Link to="/" >
+                    <img src={backIcon} alt='voltar' />
+                </Link>
+                <img src={logoImg} alt="logo" />
+            </div>
+
+            <div className="header-content">
+                <strong>
+                    {props.title}
+                </strong>
+                {props.children}
+            </div>
+
+        </div>)
+}
+
+export default PageHeader
